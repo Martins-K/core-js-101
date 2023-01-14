773a675a94cbc4467c4a9c7dc185084e47424f70
Martins-K <martins.kruklis@gmail.com>
2023-01-14 11:42:34 +0200

    feat: update numbers-tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -213,8 +213,10 @@ function roundToPowerOfTen(num, pow) {
  *   17 => true
  */
 function isPrime(n) {
-  if (n < 2) return false;
-  for (let i = 2; i <= Math.sqrt(n); i = +i) {
+  if (n <= 1) return false;
+  if (n % 2 === 0 && n > 2) return false;
+  const s = Math.sqrt(n);
+  for (let i = 3; i <= s; i += 2) {
     if (n % i === 0) return false;
   }
   return true;
